4765795d73ac94d851c788f4ee8eefc7e559a40a
M4p4 <homejr@protonmail.com>
2023-01-12 04:55:09 +0100

    update

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="referrer" content="no-referrer" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
